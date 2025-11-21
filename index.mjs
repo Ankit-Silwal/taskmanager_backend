fcd2a0466466a109257cff5738d79b80adc194bb
GubbinLubbin <silwalankit27@gmail.com>
2025-11-21 15:51:28 +0530

    Added the hashing and the logic for ogin and register

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import dotenv from "dotenv"
 import express from "express"
 import connectDB from "./src/config/db.mjs"
+import router from "./src/routes/authroutes.mjs"
 dotenv.config()
 connectDB()
 const app = express()
@@ -9,6 +10,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   return res.json({ msg: "Server is running" })
 })
+app.use('/api/auth',router)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
--- src/controller/authcontroller.mjs
+++ src/controller/authcontroller.mjs
@@ -2,8 +2,8 @@ import bcrypt from "bcrypt";
 import User from "../models/Users.mjs";
 export const registerUser = async (req, res) => {
   try {
-    const { name, password, email } = req.body;
-    if (!name || !password || !email) {
+    const { username, password, email } = req.body;
+    if (!username || !password || !email) {
       return res.json({ msg: "Please provide all the required credentials" });
     }
     const userExists = await User.findOne({ email });
@@ -12,13 +12,13 @@ export const registerUser = async (req, res) => {
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({
-      name,
+      username,
       email,
       password: hashedPassword
     });
     return res.status(201).json({
       success: true,
-      user: newUser.name,
+      user: newUser.username,
       email: newUser.email,
     });
   } catch (err) {
@@ -29,3 +29,34 @@ export const registerUser = async (req, res) => {
     });
   }
 };
+export const loginUser=async (req,res)=>{
+  try{
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        msg: "There is no user with this email"
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        msg: "Invalid credentials"
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "login successful",
+      user: {
+        username: user.username,
+        email: user.email,
+      }
+    });
+  }catch(err){
+    return res.status(500).json({
+      success:false,
+      msg:"Unable to login",
+      error:err.message
+    })
+  }
+}
